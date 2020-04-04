afed1c2a2ab5b63f43436420808d97f4faba4b83
Jonatan Hatakeyama Zeidler <jonatan_zeidler@gmx.de>
2020-04-04 09:31:08 +0200

    Remove old build and package scripts

--- package.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-mkdir package
-cp qml logo.png manifest.json pdfjsViewer.desktop pdfjsViewer.json pdfjsViewer-contenthub.json www package -rf
-cd package
-click build .
-cp *.click ..
-cd ..
-rm -rf package
